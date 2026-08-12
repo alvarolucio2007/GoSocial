d53192b8dcc7c37ffab0b6b4ea0b59f93b76cde4
Álvaro Lúcio <alvarolucio2007@gmail.com>
2026-08-11 22:54:34 -0300

    feat: Implementing incomplete server package. Will finish tomorrow (this will be replaced by Gin soon enough)

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type api struct {
+	addr string
+}
+
+func (a *api) getUsersHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	err := json.NewEncoder(w).Encode(users)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
+func (a *api) createUsersHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Created user ..."))
+}
 
 func main() {
-	fmt.Println("Hello World")
+	s := &api{addr: ":8080"}
+	mux := http.NewServeMux()
+	srv := &http.Server{
+		Addr:    s.addr,
+		Handler: mux,
+	}
+	mux.HandleFunc("GET /users", s.getUsersHandler)
+	mux.HandleFunc("POST /users", s.createUsersHandler)
+	srv.ListenAndServe()
 }
