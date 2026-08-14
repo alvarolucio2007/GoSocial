d09cb0f37f76ed1daa8b93471877a306772ce3f4
Álvaro Lúcio <alvarolucio2007@gmail.com>
2026-08-14 18:24:28 -0300

    feat: Adding Post struct, pgx and implementing create functions with real SQL.

--- internal/store/posts.go
+++ internal/store/posts.go
@@ -3,12 +3,29 @@ package store
 import (
 	"context"
 	"database/sql"
+	"time"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
-type PostsStore struct {
+type Post struct {
+	ID        int64     `json:"id"`
+	Content   string    `json:"content"`
+	Title     string    `json:"title"`
+	UserID    int64     `json:"user_id"`
+	Tags      []string  `json:"tags"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+type PostStore struct {
 	db *sql.DB
 }
 
-func (p *PostsStore) Create(ctx context.Context) error {
+func (p *PostStore) Create(ctx context.Context, post *Post) error {
+	query := `INSERT INTO posts (content,title,user_id,tags)
+									VALUES ($1,$2,$3,$4) RETURNING id,created_at,updated_at`
+	if err := p.db.QueryRowContext(ctx, query, post.Content, post.Title, post.UserID, post.Tags).Scan(&post.ID, &post.Content, &post.UpdatedAt); err != nil {
+		return err
+	}
 	return nil
 }
