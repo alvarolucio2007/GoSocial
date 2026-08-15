7624d5acd631ca825d75465ef1dcd6381a445074
Álvaro Lúcio <alvarolucio2007@gmail.com>
2026-08-15 18:17:41 -0300

    feat: Adding a couple of routes (GET and POST for posts)

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -17,6 +17,7 @@ type application struct {
 type config struct {
 	addr string
 	db   dbConfig
+	env  string
 }
 type dbConfig struct {
 	addr        string
@@ -37,6 +38,13 @@ func (app *application) mount() http.Handler {
 
 	r.Route("/v1", func(r chi.Router) {
 		r.Get("/health", app.healthCheckHandler)
+
+		r.Route("/posts", func(r chi.Router) {
+			r.Post("/", app.createPostHandler)
+			r.Route("/{postID}", func(r chi.Router) {
+				r.Get("/", app.readPostHandler)
+			})
+		})
 	})
 	return r
 }
