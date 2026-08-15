8140ee6ca419a84fe5a825ffdaead630628f3803
Álvaro Lúcio <alvarolucio2007@gmail.com>
2026-08-15 15:38:50 -0300

    feat: Implenting functions in UserRepository interface (RUD functions from CRUD) and adding them into UserStore struct.

--- internal/store/users.go
+++ internal/store/users.go
@@ -3,6 +3,7 @@ package store
 import (
 	"context"
 	"database/sql"
+	"errors"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
@@ -19,6 +20,9 @@ type UserStore struct {
 }
 type UserRepository interface {
 	Create(ctx context.Context, user *User) error
+	Read(ctx context.Context, idUser int) (*User, error)
+	Update(ctx context.Context, user *User) error
+	Delete(ctx context.Context, idUser int) error
 }
 
 func (s *UserStore) Create(ctx context.Context, user *User) error {
@@ -26,6 +30,59 @@ func (s *UserStore) Create(ctx context.Context, user *User) error {
 									VALUES ($1,$2,$3) RETURNING id,created_at`
 	if err := s.db.QueryRowContext(ctx, query, user.Username, user.Email, user.Password).Scan(&user.ID, &user.CreatedAt); err != nil {
 		return err
+	}
+	return nil
+}
+
+var ErrUserNotFound = errors.New("user not found")
+
+func (s *UserStore) Read(ctx context.Context, idUser int) (*User, error) {
+	query := `SELECT id,username,email,password,created_at FROM usuario WHERE id = $1`
+	var u User
+	err := s.db.QueryRowContext(ctx, query, idUser).Scan(&u.ID, &u.Username, &u.Email, &u.Password, &u.CreatedAt)
+	if errors.Is(err, ErrUserNotFound) {
+		return nil, ErrUserNotFound
+	}
+	if err != nil {
+		return nil, err
+	}
+	return &u, nil
+}
+
+func (s *UserStore) Update(ctx context.Context, user *User) error {
+	query := `UPDATE users
+	SET	
+		username=COALESCE(NULLIF($1,''),username),
+		email=COALESCE(NULLIF($2,''),email),
+		password=COALESCE(NULLIF($3,''),password)
+	WHERE id=$4
+	`
+	res, err := s.db.ExecContext(ctx, query, user.Username, user.Email, user.Password, user.ID)
+	if err != nil {
+		return err
+	}
+	count, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if count == 0 {
+		return ErrUserNotFound
+	}
+	return nil
+}
+
+func (s *UserStore) Delete(ctx context.Context, idUser int) error {
+	query := `DELETE FROM users WHERE id=$1`
+	res, err := s.db.ExecContext(ctx, query, idUser)
+	if err != nil {
+		return err
+	}
+	count, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if count == 0 {
+		return ErrUserNotFound
 	}
 	return nil
 }
