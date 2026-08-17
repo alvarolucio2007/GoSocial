0edc5c795d90dc905a3fac74a522f37b4c8b251f
Álvaro Lúcio <alvarolucio2007@gmail.com>
2026-08-17 17:35:52 -0300

    feat: Implementing the rest of the user handlers.

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"errors"
 	"net/http"
+	"strconv"
 
 	"github.com/alvarolucio2007/GoSocial/internal/store"
+	"github.com/go-chi/chi/v5"
 )
 
 type CreateUserPayload struct {
@@ -32,4 +35,82 @@ func (app *application) createUserHandler(w http.ResponseWriter, r *http.Request
 		_ = writeJSONError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
+}
+
+func (app *application) readUserHandler(w http.ResponseWriter, r *http.Request) {
+	idParam := chi.URLParam(r, "postID")
+	id, err := strconv.ParseInt(idParam, 10, 64)
+	if err != nil {
+		_ = writeJSONError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+	ctx := r.Context()
+	user, err := app.storage.Users.Read(ctx, int(id))
+	if err != nil {
+		switch {
+		case errors.Is(err, store.ErrUserNotFound):
+			_ = writeJSONError(w, http.StatusNotFound, err.Error())
+		default:
+			_ = writeJSONError(w, http.StatusInternalServerError, err.Error())
+		}
+		return
+	}
+	if err := writeJSON(w, http.StatusFound, &user); err != nil {
+		_ = writeJSONError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+}
+
+func (app *application) updateUserHandler(w http.ResponseWriter, r *http.Request) {
+	idParam := chi.URLParam(r, "userID")
+	id, err := strconv.ParseInt(idParam, 10, 64)
+	if err != nil {
+		_ = writeJSONError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+	var payload store.User
+	if err := readJSON(w, r, &payload); err != nil {
+		_ = writeJSONError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+	user := &store.User{
+		ID:       id,
+		Username: payload.Username,
+		Password: payload.Password,
+		Email:    payload.Email,
+	}
+	ctx := r.Context()
+	if err := app.storage.Users.Update(ctx, user); err != nil {
+		switch {
+		case errors.Is(err, store.ErrUserNotFound):
+			_ = writeJSONError(w, http.StatusNotFound, err.Error())
+		default:
+			_ = writeJSONError(w, http.StatusInternalServerError, err.Error())
+		}
+		return
+	}
+	if err := writeJSON(w, http.StatusOK, &user); err != nil {
+		_ = writeJSONError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+}
+
+func (app *application) deleteUserHandler(w http.ResponseWriter, r *http.Request) {
+	idParam := chi.URLParam(r, "userID")
+	id, err := strconv.ParseInt(idParam, 10, 64)
+	if err != nil {
+		_ = writeJSONError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+	ctx := r.Context()
+	if err := app.storage.Users.Delete(ctx, int(id)); err != nil {
+		switch {
+		case errors.Is(err, store.ErrPostNotFound):
+			_ = writeJSONError(w, http.StatusNotFound, err.Error())
+		default:
+			_ = writeJSONError(w, http.StatusInternalServerError, err.Error())
+		}
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
 }
