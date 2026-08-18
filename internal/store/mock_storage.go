6fd7599d571c3650b9532dcd338b9b2e7c9ea092
Álvaro Lúcio <alvarolucio2007@gmail.com>
2026-08-18 19:42:00 -0300

    feat: Updating mockstorage and generator func

--- internal/store/mock_storage.go
+++ internal/store/mock_storage.go
@@ -1,13 +1,15 @@
 package store
 
 type MockStorage struct {
-	Posts PostRepository
-	Users UserRepository
+	Posts    PostRepository
+	Users    UserRepository
+	Comments CommentRepository
 }
 
-func NewMockStorage(mapPost map[int]*Post, mapUser map[int]*User) MockStorage {
+func NewMockStorage(mapPost map[int]*Post, mapUser map[int]*User, mapComments map[int]*Comment) MockStorage {
 	return MockStorage{
-		Posts: &MockPostRepository{posts: mapPost},
-		Users: &MockUserRepository{users: mapUser},
+		Posts:    &MockPostRepository{posts: mapPost},
+		Users:    &MockUserRepository{users: mapUser},
+		Comments: &MockCommentRepository{comments: mapComments},
 	}
 }
