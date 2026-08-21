00c8e8a370c237eb70132a01ad93582762629fb7
Álvaro Lúcio <alvarolucio2007@gmail.com>
2026-08-21 14:56:18 -0300

    fix: adding logs for errors and defers

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -7,15 +7,21 @@ import (
 
 func (app *application) internalServerError(w http.ResponseWriter, r *http.Request, err error) {
 	log.Printf("internal error:%s path: %s,error: %s\n", r.Method, r.URL.Path, err)
-	writeJSONError(w, http.StatusInternalServerError, " the server has encountered a problem.")
+	if err := writeJSONError(w, http.StatusInternalServerError, " the server has encountered a problem."); err != nil {
+		log.Printf("error inside internalServerError func: %v", err)
+	}
 }
 
 func (app *application) badRequestError(w http.ResponseWriter, r *http.Request, err error) {
 	log.Printf("bad request error:%s path: %s,error: %s\n", r.Method, r.URL.Path, err)
-	writeJSONError(w, http.StatusInternalServerError, err.Error())
+	if err := writeJSONError(w, http.StatusBadRequest, err.Error()); err != nil {
+		log.Printf("error inside BadRequestError func: %v", err)
+	}
 }
 
 func (app *application) notFoundError(w http.ResponseWriter, r *http.Request, err error) {
 	log.Printf("not found error:%s path: %s,error: %s\n", r.Method, r.URL.Path, err)
-	writeJSONError(w, http.StatusNotFound, "resource not found")
+	if err := writeJSONError(w, http.StatusNotFound, "resource not found"); err != nil {
+		log.Printf("error inside StatusNotFound func: %v", err)
+	}
 }
