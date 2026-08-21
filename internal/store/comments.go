e5ba5cb66343970124e5ed1b02da37fa12f9fc13
Álvaro Lúcio <alvarolucio2007@gmail.com>
2026-08-21 11:09:55 -0300

    feat: Added create comment func

--- internal/store/comments.go
+++ internal/store/comments.go
@@ -29,6 +29,23 @@ type CommentStore struct {
 }
 type CommentRepository interface {
 	GetByPostID(ctx context.Context, postID int64) ([]Comment, error)
+	Create(ctx context.Context, comment *Comment) error
+}
+
+func (s *CommentStore) Create(ctx context.Context, comment *Comment) error {
+	query := `INSERT INTO comments (post_id,user_id,content)
+	VALUES ($1,$2,$3)
+	RETURNING id,created_at
+	`
+	ctx, cancel := context.WithTimeout(ctx, QueryTimeout)
+	defer cancel()
+	err := s.db.QueryRowContext(ctx, query, comment.PostID, comment.UserID, comment.Content).Scan(
+		&comment.ID, &comment.CreatedAt,
+	)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (s *CommentStore) GetByPostID(ctx context.Context, postID int64) ([]Comment, error) {
