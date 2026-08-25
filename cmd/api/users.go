81a3a8789d2f2d35b862a0ca1e2602851cae7ff3
Álvaro Lúcio <alvarolucio2007@gmail.com>
2026-08-25 11:32:21 -0300

    feat: Upgrading middleware to be able to inject users from context.

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"net/http"
 	"strconv"
@@ -8,6 +9,10 @@ import (
 	"github.com/alvarolucio2007/GoSocial/internal/store"
 	"github.com/go-chi/chi/v5"
 )
+
+type userKey string
+
+const userCtx userKey = "user"
 
 type CreateUserPayload struct {
 	Username string `json:"username" validate:"required,max=100"`
@@ -41,23 +46,7 @@ func (app *application) createUserHandler(w http.ResponseWriter, r *http.Request
 }
 
 func (app *application) readUserHandler(w http.ResponseWriter, r *http.Request) {
-	idParam := chi.URLParam(r, "userID")
-	id, err := strconv.ParseInt(idParam, 10, 64)
-	if err != nil {
-		app.badRequestError(w, r, err)
-		return
-	}
-	ctx := r.Context()
-	user, err := app.storage.Users.Read(ctx, int(id))
-	if err != nil {
-		switch {
-		case errors.Is(err, store.ErrUserNotFound):
-			app.notFoundError(w, r, err)
-		default:
-			app.internalServerError(w, r, err)
-		}
-		return
-	}
+	user := getUserFromContext(r)
 	if err := app.jsonResponse(w, http.StatusOK, &user); err != nil {
 		app.internalServerError(w, r, err)
 		return
@@ -125,4 +114,63 @@ func (app *application) deleteUserHandler(w http.ResponseWriter, r *http.Request
 		return
 	}
 	w.WriteHeader(http.StatusNoContent)
+}
+
+type FollowUser struct {
+	UserID int64 `json:"user_id"`
+}
+
+func (app *application) followUserHandler(w http.ResponseWriter, r *http.Request) {
+	followerUser := getUserFromContext(r)
+	var payload FollowUser
+	if err := readJSON(w, r, &payload); err != nil {
+		app.badRequestError(w, r, err)
+		return
+	}
+	ctx := r.Context()
+	if err := app.storage.Followers.Follow(ctx, followerUser.ID, payload.UserID); err != nil {
+		app.internalServerError(w, r, err)
+		return
+	}
+	if err := app.jsonResponse(w, http.StatusNoContent, nil); err != nil {
+		app.internalServerError(w, r, err)
+		return
+	}
+}
+
+func (app *application) unfollowUserHandler(w http.ResponseWriter, r *http.Request) {
+	user := getUserFromContext(r)
+	if err := app.jsonResponse(w, http.StatusNoContent, &user); err != nil {
+		app.internalServerError(w, r, err)
+		return
+	}
+}
+
+func (app *application) userContextMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		idParam := chi.URLParam(r, "userID")
+		id, err := strconv.ParseInt(idParam, 10, 64)
+		if err != nil {
+			app.badRequestError(w, r, err)
+			return
+		}
+		ctx := r.Context()
+		user, err := app.storage.Users.Read(ctx, int(id))
+		if err != nil {
+			switch {
+			case errors.Is(err, store.ErrUserNotFound):
+				app.notFoundError(w, r, err)
+			default:
+				app.internalServerError(w, r, err)
+			}
+			return
+		}
+		ctx = context.WithValue(ctx, userCtx, user)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+func getUserFromContext(r *http.Request) *store.User {
+	user, _ := r.Context().Value(userCtx).(*store.User)
+	return user
 }
