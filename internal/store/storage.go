d39f96590079dc7321fa3ebc0ab60c54fd7b4cdb
Álvaro Lúcio <alvarolucio2007@gmail.com>
2026-08-25 11:31:37 -0300

    feat: Updating storage struct and NewPGStorage func to add FollowerRepository

--- internal/store/storage.go
+++ internal/store/storage.go
@@ -6,16 +6,18 @@ import (
 )
 
 type Storage struct {
-	Posts    PostRepository
-	Users    UserRepository
-	Comments CommentRepository
+	Posts     PostRepository
+	Users     UserRepository
+	Comments  CommentRepository
+	Followers FollowerRepository
 }
 
 func NewPostgresStorage(db *sql.DB) Storage {
 	return Storage{
-		Posts:    &PostStore{db: db},
-		Users:    &UserStore{db: db},
-		Comments: &CommentStore{db: db},
+		Posts:     &PostStore{db: db},
+		Users:     &UserStore{db: db},
+		Comments:  &CommentStore{db: db},
+		Followers: &FollowerStore{db: db},
 	}
 }
 
