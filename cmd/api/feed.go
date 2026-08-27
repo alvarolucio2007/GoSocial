7fde3c17b2053e08a1a5c53da109eaec844fd9e8
Álvaro Lúcio <alvarolucio2007@gmail.com>
2026-08-27 10:42:56 -0300

    feat: Adding pagination, and changing ID in GetUserFeed (need to actually implement that)

--- cmd/api/feed.go
+++ cmd/api/feed.go
@@ -1,10 +1,28 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/alvarolucio2007/GoSocial/internal/store"
+)
 
 func (app *application) getUserFeedHandler(w http.ResponseWriter, r *http.Request) {
+	fq := store.PaginatedFeedQuery{
+		Limit:  20,
+		Offset: 0,
+		Sort:   "desc",
+	}
+	fq, err := fq.Parse(r)
+	if err != nil {
+		app.badRequestError(w, r, err)
+		return
+	}
+	if err := Validate.Struct(fq); err != nil {
+		app.badRequestError(w, r, err)
+		return
+	}
 	ctx := r.Context()
-	feed, err := app.storage.Posts.GetUserFeed(ctx, int64(100))
+	feed, err := app.storage.Posts.GetUserFeed(ctx, int64(10), fq)
 	if err != nil {
 		app.internalServerError(w, r, err)
 		return
