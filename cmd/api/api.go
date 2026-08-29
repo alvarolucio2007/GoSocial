a705fc393146a4b90a90b19320afaf56450d63c3
Álvaro Lúcio <alvarolucio2007@gmail.com>
2026-08-29 20:43:58 -0300

    feat: Implementing logger.

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"time"
 
@@ -11,11 +10,13 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	httpSwagger "github.com/swaggo/http-swagger/v2"
+	"go.uber.org/zap"
 )
 
 type application struct {
 	config  config
 	storage store.Storage
+	logger  *zap.SugaredLogger
 }
 type config struct {
 	addr   string
@@ -87,6 +88,6 @@ func (app *application) run(mux http.Handler) error {
 		ReadTimeout:  10 * time.Second,
 		IdleTimeout:  time.Minute,
 	}
-	log.Printf("server has started at %s", app.config.addr)
+	app.logger.Infow("server has started", "addr", app.config.addr, "env", app.config.env)
 	return srv.ListenAndServe()
 }
