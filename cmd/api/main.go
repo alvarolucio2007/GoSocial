bfe9607e713048e4686f1a2af44c93e4e86229bf
Álvaro Lúcio <alvarolucio2007@gmail.com>
2026-08-29 20:43:44 -0300

    feat: Adding logger and implementing.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"log"
 	"time"
 
 	"github.com/alvarolucio2007/GoSocial/internal/db"
 	"github.com/alvarolucio2007/GoSocial/internal/env"
 	"github.com/alvarolucio2007/GoSocial/internal/store"
 	_ "github.com/swaggo/http-swagger/v2"
+	"go.uber.org/zap"
 )
 
 const version = "0.0.1"
@@ -29,11 +29,17 @@ const version = "0.0.1"
 // @name						Authorization
 // @description
 func main() {
+	logger := zap.Must(zap.NewProduction()).Sugar()
+	defer func() {
+		if err := logger.Sync(); err != nil {
+			logger.Errorf("Couldn't sync logger: %s", err)
+		}
+	}()
 	maxOpenConn, _ := env.GetInt("DB_MAX_OPEN_CONN", 30)
 	maxIdleConn, _ := env.GetInt("DB_MAX_IDLE_CONN", 30)
 	maxIdleTime, err := time.ParseDuration(env.GetString("DB_MAX_IDLE_TIME", "15m"))
 	if err != nil {
-		log.Panicf("PANIC: couldn't parse maxIdleTime, error: %v", err) // might want to deal w this later oh well
+		logger.Errorf("couldn't parse maxIdleTime, error: %v", err) // might want to deal w this later oh well
 	}
 
 	cfg := config{
@@ -49,19 +55,20 @@ func main() {
 	}
 	db, err := db.New(cfg.db.addr, cfg.db.maxOpenConn, cfg.db.maxIdleConn, cfg.db.maxIdleTime)
 	if err != nil {
-		log.Panicf("PANIC: couldn't connect to database, error: %v", err)
+		logger.Panicf("PANIC: couldn't connect to database, error: %v", err)
 	}
 	defer func() {
 		if err := db.Close(); err != nil {
-			log.Printf("couldn't close the database: %v\n", err)
+			logger.Errorf("couldn't close the database: %v\n", err)
 		}
 	}()
-
 	store := store.NewPostgresStorage(db)
+
 	app := &application{
-		cfg,
-		store,
+		config:  cfg,
+		storage: store,
+		logger:  logger,
 	}
 	mux := app.mount()
-	log.Fatal(app.run(mux))
+	logger.Fatal(app.run(mux))
 }
