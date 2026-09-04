e7c3b586bad55270d2bd4e03ff0800e0fce079a8
Álvaro Lúcio <alvarolucio2007@gmail.com>
2026-09-03 23:16:11 -0300

    fix: fixing swagger doc and adding SHA256 tokens.

--- cmd/api/auth.go
+++ cmd/api/auth.go
@@ -15,21 +15,26 @@ type RegisterUserPayload struct {
 	Password string `json:"password" validate:"required,min=3,max=72"`
 }
 
+type UserWithToken struct {
+	*store.User
+	Token string `json:"token"`
+}
+
 // registerUserHandler godoc
 //
-// @Summary Registers an user
-// @Description Registers an user
-// @Tags authentication
-// @Accept json
-// @Produce json
-// @Param payload body RegisterUserPayload true "User credentials"
-// @Success 201 {object} store.User "User registered"
-// @Failure 400 {object} error
-// @Failure 500 {object} error
-// @Router /authentication/user [post]
+//	@Summary		Registers an user
+//	@Description	Registers an user
+//	@Tags			authentication
+//	@Accept			json
+//	@Produce		json
+//	@Param			payload	body		RegisterUserPayload	true	"User credentials"
+//	@Success		201		{object}	UserWithToken		"User registered"
+//	@Failure		400		{object}	error
+//	@Failure		500		{object}	error
+//	@Router			/authentication/user [post]
 func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Request) {
 	var payload RegisterUserPayload
-	if err := readJSON(w, r, payload); err != nil {
+	if err := readJSON(w, r, &payload); err != nil {
 		app.badRequestError(w, r, err)
 		return
 	}
@@ -61,9 +66,13 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		}
 		return
 	}
+	userWithToken := UserWithToken{
+		User:  user,
+		Token: plainToken,
+	}
 
 	// TODO: Send e-mail
-	if err := app.jsonResponse(w, http.StatusCreated, nil); err != nil {
+	if err := app.jsonResponse(w, http.StatusCreated, userWithToken); err != nil {
 		app.internalServerError(w, r, err)
 		return
 	}
