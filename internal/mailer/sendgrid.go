5ae263a2a72352127264b6501839e0cb06a8a323
Álvaro Lúcio <alvarolucio2007@gmail.com>
2026-09-07 22:17:33 -0300

    fix: removing log from Send, also making it return int

--- internal/mailer/sendgrid.go
+++ internal/mailer/sendgrid.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"fmt"
 	"html/template"
-	"log"
 	"time"
 
 	"github.com/sendgrid/sendgrid-go"
@@ -22,24 +21,24 @@ func NewSendGrid(apiKey, fromEmail string) *SendGridMailer {
 	return &SendGridMailer{fromEmail: fromEmail, apiKey: apiKey, client: client}
 }
 
-func (m *SendGridMailer) Send(templateFile, username, email string, data any, isSandbox bool) error {
+func (m *SendGridMailer) Send(templateFile, username, email string, data any, isSandbox bool) (int, error) {
 	from := mail.NewEmail(FromName, m.fromEmail)
 	to := mail.NewEmail(username, email)
 
 	tmpl, err := template.ParseFS(FS, "templates/"+templateFile)
 	if err != nil {
-		return err
+		return 0, err
 	}
 
 	subject := new(bytes.Buffer)
 	err = tmpl.ExecuteTemplate(subject, "subject", data)
 	if err != nil {
-		return err
+		return 0, err
 	}
 	body := new(bytes.Buffer)
 	err = tmpl.ExecuteTemplate(body, "body", data)
 	if err != nil {
-		return err
+		return 0, err
 	}
 	message := mail.NewSingleEmail(from, subject.String(), to, "", body.String())
 
@@ -50,16 +49,14 @@ func (m *SendGridMailer) Send(templateFile, username, email string, data any, is
 			},
 		},
 	)
-
+	var errRetry error
 	for i := range maxRetries {
-		response, err := m.client.Send(message)
-		if err != nil {
-			log.Printf("failed to send email to %v, attempt %d of %d,error %v", email, i+1, maxRetries, err)
+		response, retryErr := m.client.Send(message)
+		if retryErr != nil {
 			time.Sleep(time.Second * time.Duration(i+1))
 			continue
 		}
-		log.Printf("email sent with status code %v", response.StatusCode)
-		return nil
+		return response.StatusCode, nil
 	}
-	return fmt.Errorf("failed to send email after %d attempts", maxRetries)
+	return 0, fmt.Errorf("failed to send email after %d attempts, error: %v", maxRetries, errRetry.Error())
 }
