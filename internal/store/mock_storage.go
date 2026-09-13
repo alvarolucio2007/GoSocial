ccba325e9926504c89a8a3bf921751f63aa39278
Álvaro Lúcio <alvarolucio2007@gmail.com>
2026-09-12 23:35:07 -0300

    feat: Removing MockStorage (was the same as Storage) and making NewMockStorage return Storage

--- internal/store/mock_storage.go
+++ internal/store/mock_storage.go
@@ -1,17 +1,11 @@
 package store
 
-type MockStorage struct {
-	Posts     PostRepository
-	Users     UserRepository
-	Comments  CommentRepository
-	Followers FollowerRepository
-	Roles     RolesRepository
-}
-
-func NewMockStorage(mapPost map[int]*Post, mapUser map[int]*User, mapComments map[int]*Comment) MockStorage {
-	return MockStorage{
-		Posts:    &MockPostRepository{posts: mapPost},
-		Users:    &MockUserRepository{users: mapUser},
-		Comments: &MockCommentRepository{comments: mapComments},
+func NewMockStorage(mapPost map[int]*Post, mapUser map[int]*User, mapComments map[int]*Comment, mapFollowers map[int]*Follower, mapRoles map[int]*Role) Storage {
+	return Storage{
+		Posts:     &MockPostRepository{posts: mapPost},
+		Users:     &MockUserRepository{users: mapUser},
+		Comments:  &MockCommentRepository{comments: mapComments},
+		Followers: &MockFollowerRepository{followers: mapFollowers},
+		Roles:     &MockRoleRepository{roles: mapRoles},
 	}
 }
