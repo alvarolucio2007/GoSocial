6f09c7f1279f7fe4284264054443d13c2b30687b
Álvaro Lúcio <alvarolucio2007@gmail.com>
2026-09-14 14:14:40 -0300

    feat: Adding proper auth mocking.

--- internal/auth/mock_auth.go
+++ internal/auth/mock_auth.go
@@ -1,15 +1,25 @@
 package auth
 
-type MockAuthenticator struct{}
+import "uuid"
+
+type MockAuthenticator struct{ tokens map[string]*Claims }
 
 func NewMockAuthenticator() *MockAuthenticator {
-	return &MockAuthenticator{}
+	return &MockAuthenticator{
+		tokens: make(map[string]*Claims),
+	}
 }
 
 func (m *MockAuthenticator) CreateToken(claims Claims) (string, error) {
-	return "", nil
+	token := uuid.New().String()
+	m.tokens[token] = &claims
+	return token, nil
 }
 
 func (m *MockAuthenticator) VerifyToken(token string) (*Claims, error) {
-	return nil, nil
+	claims, ok := m.tokens[token]
+	if !ok {
+		return nil, ErrInvalidToken
+	}
+	return claims, nil
 }
