7e11703344dbf3f0c0405dfeb0ad5aeab8212406
Álvaro Lúcio <alvarolucio2007@gmail.com>
2026-09-17 18:23:00 -0300

    fix: Fixing NewMockStorage returns (adding required fields to posts and comments)

--- internal/store/mock_storage.go
+++ internal/store/mock_storage.go
@@ -2,9 +2,9 @@ package store
 
 func NewMockStorage(mapPost map[int64]*Post, mapUser map[int64]*User, mapComments map[int64]*Comment, mapFollowers map[FollowKey]struct{}, mapRoles map[int]*Role) Storage {
 	return Storage{
-		Posts:     &MockPostRepository{posts: mapPost},
+		Posts:     &MockPostRepository{posts: mapPost, users: mapUser},
 		Users:     &MockUserRepository{users: mapUser},
-		Comments:  &MockCommentRepository{comments: mapComments},
+		Comments:  &MockCommentRepository{comments: mapComments, posts: mapPost},
 		Followers: &MockFollowerRepository{followers: mapFollowers},
 		Roles:     &MockRoleRepository{roles: mapRoles},
 	}
