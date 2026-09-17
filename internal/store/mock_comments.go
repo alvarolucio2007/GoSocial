536835a9c75afbf5498a043380088a614a94ded5
Álvaro Lúcio <alvarolucio2007@gmail.com>
2026-09-17 17:14:39 -0300

    mock: Adding comments mock

--- internal/store/mock_comments.go
+++ internal/store/mock_comments.go
@@ -1,15 +1,31 @@
 package store
 
-import "context"
+import (
+	"context"
+)
 
 type MockCommentRepository struct {
 	comments map[int64]*Comment
+	posts    map[int64]*Post
 }
 
 func (m *MockCommentRepository) GetByPostID(ctx context.Context, postID int64) ([]Comment, error) {
-	return nil, nil
+	post, exists := m.posts[postID]
+	if !exists {
+		return nil, ErrNotFound
+	}
+	result := make([]Comment, len(post.Comments))
+	for c := range post.Comments {
+		result = append(result, *m.comments[int64(c)])
+	}
+	return result, nil
 }
 
 func (m *MockCommentRepository) Create(ctx context.Context, comment *Comment) error {
+	_, exists := m.comments[comment.ID]
+	if !exists {
+		return ErrConflict
+	}
+	m.comments[comment.ID] = comment
 	return nil
 }
