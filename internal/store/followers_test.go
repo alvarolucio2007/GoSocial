5806220997d21c73fc0f3570b4978d9b4e3077cd
Álvaro Lúcio <alvarolucio2007@gmail.com>
2026-09-20 21:58:38 -0300

    test: Refactor tests to be less verbose by utilizing helper functions

--- internal/store/followers_test.go
+++ internal/store/followers_test.go
@@ -9,24 +9,14 @@ import (
 
 func TestFollowerStore_Follow(t *testing.T) {
 	user1 := &User{Username: "testFollow", Email: "testFollow@gmail.com"}
-	tx, err := testDB.Begin()
-	require.NoError(t, err)
-	err = testStore.Users.Create(t.Context(), tx, user1)
-	require.NoError(t, err)
-	_, err = testDB.Exec("UPDATE users SET is_active = true WHERE username='testFollow'")
-	require.NoError(t, err)
+	createUserTest(t, user1)
 	user2 := &User{Username: "testFollowed", Email: "testFollowed@gmail.com"}
-	err = testStore.Users.Create(t.Context(), tx, user2)
-	require.NoError(t, err)
-	_, err = testDB.Exec("UPDATE users SET is_active = true WHERE username='testFollowed'")
-	require.NoError(t, err)
-	err = tx.Commit()
-	require.NoError(t, err)
+	createUserTest(t, user2)
 	t.Cleanup(func() {
 		ctx := context.Background()
-		err = testStore.Users.Delete(ctx, 1)
+		err := testStore.Users.Delete(ctx, user1.ID)
 		require.NoError(t, err)
-		err = testStore.Users.Delete(ctx, 2)
+		err = testStore.Users.Delete(ctx, user2.ID)
 		require.NoError(t, err)
 	})
 	tests := []struct {
@@ -86,6 +76,34 @@ func TestFollowerStore_Follow(t *testing.T) {
 			}
 			err := testStore.Followers.Unfollow(t.Context(), 1, 2)
 			require.NoError(t, err)
+		})
+	}
+}
+
+func TestFollowerStore_Unfollow(t *testing.T) {
+	tests := []struct {
+		name string // description of this test case
+		// Named input parameters for target function.
+		userID     int64
+		followerID int64
+		wantErr    bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// TODO: construct the receiver type.
+			var s FollowerStore
+			gotErr := s.Unfollow(t.Context(), tt.userID, tt.followerID)
+			if gotErr != nil {
+				if !tt.wantErr {
+					t.Errorf("Unfollow() failed: %v", gotErr)
+				}
+				return
+			}
+			if tt.wantErr {
+				t.Fatal("Unfollow() succeeded unexpectedly")
+			}
 		})
 	}
 }
