f6ec9eec7def33fc9e84f4c1e4782854eeb1bc6e
Álvaro Lúcio <alvarolucio2007@gmail.com>
2026-09-21 22:19:55 -0300

    test: Adding TestCommentStore create test

--- internal/store/comments_test.go
+++ internal/store/comments_test.go
@@ -1,31 +1,86 @@
 package store
 
 import (
+	"context"
 	"testing"
+
+	"github.com/go-openapi/testify/require"
 )
 
 func TestCommentStore_Create(t *testing.T) {
+	user := &User{Username: "testCommentUser", Email: "testCommentUser@gmail.com"}
+	createUserTest(t, user)
+
+	post := &Post{UserID: user.ID, Content: "test post"}
+	createPostTest(t, post)
+
+	t.Cleanup(func() {
+		ctx := context.Background()
+		_, err := testDB.ExecContext(ctx, "DELETE FROM comments WHERE user_id = $1", user.ID)
+		require.NoError(t, err)
+		_, err = testDB.ExecContext(ctx, "DELETE FROM posts WHERE id = $1", post.ID)
+		require.NoError(t, err)
+		require.NoError(t, testStore.Users.Delete(ctx, user.ID))
+	})
+
 	tests := []struct {
-		name string // description of this test case
-		// Named input parameters for target function.
+		name    string
 		comment *Comment
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "valid comment",
+			comment: &Comment{
+				PostID:  post.ID,
+				UserID:  user.ID,
+				Content: "hello world",
+			},
+			wantErr: false,
+		},
+		{
+			name: "invalid postID",
+			comment: &Comment{
+				PostID:  post.ID + 99999,
+				UserID:  user.ID,
+				Content: "orphan comment",
+			},
+			wantErr: true,
+		},
+		{
+			name: "invalid userID",
+			comment: &Comment{
+				PostID:  post.ID,
+				UserID:  user.ID + 99999,
+				Content: "ghost user",
+			},
+			wantErr: true,
+		},
+		{
+			name: "empty content",
+			comment: &Comment{
+				PostID:  post.ID,
+				UserID:  user.ID,
+				Content: "",
+			},
+			wantErr: true,
+		},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			// TODO: construct the receiver type.
-			gotErr := testStore.Comments.Create(t.Context(), tt.comment)
-			if gotErr != nil {
-				if !tt.wantErr {
-					t.Errorf("Create() failed: %v", gotErr)
-				}
+			err := testStore.Comments.Create(t.Context(), tt.comment)
+
+			if tt.wantErr {
+				require.Errorf(t, err, "required error in %v function", tt.name)
 				return
 			}
-			if tt.wantErr {
-				t.Fatal("Create() succeeded unexpectedly")
+			if err != nil {
+				require.NoErrorf(t, err, "required no error in %v function", tt.name)
 			}
+
+			require.NotZero(t, tt.comment.ID, "Create should populate ID via RETURNING")
+			require.Equal(t, tt.comment.PostID, tt.comment.PostID)
+			require.Equal(t, tt.comment.UserID, tt.comment.UserID)
 		})
 	}
 }
