17205b9fc2b339f5a67e7bc6ec9b4794549e000d
Álvaro Lúcio <alvarolucio2007@gmail.com>
2026-09-23 11:18:43 -0300

    test: Adding redis connection testing.Main archive.

---
+++ internal/store/cache/utils_test.go
@@ -0,0 +1,59 @@
+package cache
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"testing"
+
+	goRedis "github.com/redis/go-redis/v9"
+	"github.com/testcontainers/testcontainers-go/modules/redis"
+)
+
+var (
+	testRedis *goRedis.Client
+	testCache Storage
+)
+
+func TestMain(m *testing.M) {
+	ctx := context.Background()
+	var testContainer *redis.RedisContainer
+	var err error
+	testRedis, testContainer, err = setupTestCache()
+	if err != nil {
+		log.Fatalf("unable to create redis and testcontainers %v", err)
+	}
+	if testRedis == nil {
+		log.Fatalf("testRedis is nil\n")
+	}
+	if testContainer == nil {
+		log.Fatalf("testContainer Redis is nil")
+	}
+	testCache = NewRedisStorage(testRedis)
+	code := m.Run()
+	if err := testContainer.Terminate(ctx); err != nil {
+		log.Printf("failed to terminate container: %v", err)
+	}
+	if err := testRedis.Close(); err != nil {
+		log.Printf("failed to terminate DB: %v", err)
+	}
+	os.Exit(code)
+}
+
+func setupTestCache() (*goRedis.Client, *redis.RedisContainer, error) {
+	ctx := context.Background()
+	rdc, err := redis.Run(ctx, "redis:8-alpine")
+	if err != nil {
+		return nil, nil, err
+	}
+	connStr, err := rdc.ConnectionString(ctx)
+	if err != nil {
+		return nil, nil, err
+	}
+	rdb := New(connStr, "", 0)
+	if rdb == nil {
+		return nil, nil, fmt.Errorf("rdb is nil at setupTestCache")
+	}
+	return rdb, rdc, nil
+}
